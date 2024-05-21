51a6fc182d014ed3a69a60225dfcd0762ae4e5cd
Abiezaptr <abieza.eresha@gmail.com>
2024-05-21 11:18:50 +0700

    bug fix control pause and countdown

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -11,72 +11,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-<!-- <script>
-    $(document).ready(function() {
-        var remainingSeconds = 6; // Ubah waktu timer menjadi 6 detik
-        var countdownInterval;
-        var isPaused = false;
-
-        function startCountdown() {
-            var countdownElement = document.getElementById('countdown');
-            var currentSeconds = remainingSeconds;
-
-            countdownInterval = setInterval(function() {
-                if (!isPaused) {
-                    countdownElement.innerHTML = '<div class="countdown-badge">' + currentSeconds + '</div>';
-                    currentSeconds--;
-
-                    if (currentSeconds < 0) {
-                        clearInterval(countdownInterval);
-                        var currentSlideIndex = $('.carousel-item.active').index();
-                        var totalSlides = $('.carousel-item').length;
-                        var nextSlideIndex = (currentSlideIndex + 1) % totalSlides;
-
-                        // Memastikan perubahan slide terjadi setelah countdown mencapai 0
-                        setTimeout(function() {
-                            $('#carouselExampleControls').carousel(nextSlideIndex);
-                            startCountdown(); // Mulai ulang countdown setelah pergantian slide
-                        }, 1000);
-
-                        remainingSeconds = 6; // Reset timer ke 6 detik setelah mencapai nol
-                    }
-                }
-            }, 1000);
-        }
-
-        function updateSlideInfo(currentSlide, totalSlides) {
-            var slideInfoElement = document.getElementById('slide-info');
-            slideInfoElement.textContent = 'Slide ' + currentSlide + ' of ' + totalSlides;
-        }
-
-        $('#pauseRunButton').on('click', function() {
-            isPaused = !isPaused; // Toggle isPaused
-            var buttonIcon = isPaused ? '<i class="fa-solid fa-circle-play"></i>' : '<i class="fa-solid fa-circle-pause"></i>';
-            $(this).html(buttonIcon);
-        });
-
-        function reloadSlideData() {
-            // Simulate page refresh with a loader
-            location.reload(true);
-        }
-
-        $('#carouselExampleControls').on('slid.bs.carousel', function(event) {
-            var totalSlides = $('.carousel-item').length;
-            var currentSlide = $(this).find('.carousel-item.active').index() + 1;
-            clearInterval(countdownInterval);
-            startCountdown();
-            updateSlideInfo(currentSlide, totalSlides);
-
-            // Check if current slide is the first slide
-            if (currentSlide === 1) {
-                reloadSlideData(); // Reload data if it's the first slide
-            }
-        });
-
-        startCountdown();
-        updateSlideInfo(1, $('.carousel-item').length);
-    });
-</script> -->
 
 <script>
     $(document).ready(function() {
@@ -125,7 +59,9 @@
 
             if (isPaused) {
                 clearInterval(countdownInterval); // Hentikan countdown saat di-pause
+                $('#countdown').hide(); // Sembunyikan countdown
             } else {
+                $('#countdown').show(); // Tampilkan countdown
                 startCountdown(); // Mulai ulang countdown saat di-unpause
             }
         });
